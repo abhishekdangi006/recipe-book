97eee503246590222946263ae642b1dfacf601e6
Abhishek Dangi <abhishekdangi006@gmail.com>
2023-08-08 18:16:58 +0530

    project completed

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
+import Cuisine from './Cuisine';
+import Searchresult from './Searchresult';
+import Recipe from './Recipe';
 
 function Pages() {
   return (
     <div>
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cuisine/:type' element={<Cuisine/>}/>
+        <Route path='/searchresult/:search' element={<Searchresult/>} />
+        <Route path='/recipe/:id' element={<Recipe/>}/>
+      </Routes>
     </div>
   )
 }
